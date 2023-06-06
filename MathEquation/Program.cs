5001f568af8bef6a38a3af5d865a32ed3b1b77ac
Otar <86528840+Blazkowich@users.noreply.github.com>
2023-06-06 13:59:58 +0400

    Update Program.cs

--- MathEquation/Program.cs
+++ MathEquation/Program.cs
@@ -4,10 +4,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        double a = UserInteractionHelper.GetCoefficient("A");
-        double b = UserInteractionHelper.GetCoefficient("B");
-        double c = UserInteractionHelper.GetCoefficient("C");
-
-        EquationHelper.SolveQuadraticEquation(a, b, c);
+        UserInteractionHelper.GetInputAndResult();
     }
 }
